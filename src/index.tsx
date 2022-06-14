cd8790975891efe7c14ba00a743f47efc5d98e15
aqilfiqran <aqilfiqran19@gmail.com>
2022-06-14 22:51:11 +0700

    chore: update to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './page/index';
-import reportWebVitals from './reportWebVitals';
-import { theme } from './style/Theme';
-import { ThemeProvider } from 'styled-components';
+import React from "react";
+import { App } from "./page/index";
+import reportWebVitals from "./reportWebVitals";
+import { theme } from "./style/Theme";
+import { ThemeProvider } from "styled-components";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
